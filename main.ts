116713e62fcddb81ef25e0b59fa2d7991e5de9ef
LEEMYOUNGBAK <94684957+LEEMYOUNGBAK@users.noreply.github.com>
2023-03-29 17:07:03 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
 OLED.init(128, 64)
 basic.forever(function () {
-    OLED.writeNumNewLine(Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P1))
+    OLED.writeStringNewLine("" + Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P1) + "cm")
+    basic.pause(500)
+    OLED.clear()
+    OLED.drawLine(
+    0,
+    32,
+    Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_cm, DigitalPin.P1),
+    32
+    )
 })
